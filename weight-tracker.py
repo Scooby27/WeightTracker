ecafddce503e716307f02773d31ac694afdaa618
Scott Boyd <scottboyd@Scotts-MacBook-Pro.local>
2018-01-27 17:24:12 +0000

    Now lists weights in correct date order

--- weight-tracker.py
+++ weight-tracker.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 
 class Calendar:
     weight_entries = []
@@ -7,10 +8,20 @@ class Calendar:
         self.weight_entries = weight_entries
         self.date_entries = date_entries
     
+    def sort_dates (self, element):
+        return element[0]
+
     def print_calendar(self):
         values = []
         for i, value in enumerate(self.date_entries):
-            values.append(value[6:] + "/" + value[4:6] + "/" + value[:4] + ": " + self.weight_entries[i])
+            date = datetime.strptime(value, "%Y%M%d")
+            values.append([date, self.weight_entries[i]])
+        values.sort(key=self.sort_dates)
+        for value in values:
+            print value[0].strftime('%d/%M/%Y') + ": " + value[1]
+        
+
+            
 
 def parse_calendar():
     file_lines  = open("Weight.ics", "r").readlines()
