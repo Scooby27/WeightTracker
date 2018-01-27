b368e37625cc292e3431bb230cbac07511b42143
Scott Boyd <scottboyd@Scotts-MacBook-Pro.local>
2018-01-27 17:25:41 +0000

    Separate Calendar to separate file

--- weight-tracker.py
+++ weight-tracker.py
@@ -1,27 +1,4 @@
-from datetime import datetime
-
-class Calendar:
-    weight_entries = []
-    date_entries = []
-
-    def __init__(self, weight_entries, date_entries):
-        self.weight_entries = weight_entries
-        self.date_entries = date_entries
-    
-    def sort_dates (self, element):
-        return element[0]
-
-    def print_calendar(self):
-        values = []
-        for i, value in enumerate(self.date_entries):
-            date = datetime.strptime(value, "%Y%M%d")
-            values.append([date, self.weight_entries[i]])
-        values.sort(key=self.sort_dates)
-        for value in values:
-            print value[0].strftime('%d/%M/%Y') + ": " + value[1]
-        
-
-            
+from Calendar import Calendar            
 
 def parse_calendar():
     file_lines  = open("Weight.ics", "r").readlines()
